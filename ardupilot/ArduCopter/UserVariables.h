e28c6273095fc9088f42ccf735f47dcb0390a5f7
David Gitz <davidgitz@gmail.com>
2014-02-04 14:36:19 -0600

    Transitioned FC code to separate Repo

---
+++ ardupilot/ArduCopter/UserVariables.h
@@ -0,0 +1,20 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+// user defined variables
+
+// example variables used in Wii camera testing - replace with your own
+// variables
+#ifdef USERHOOK_VARIABLES
+int MODE_PIN = 59; //AN5
+int ARMDISARM_PIN = 58; //AN4 
+#if WII_CAMERA == 1
+WiiCamera           ircam;
+int                 WiiRange=0;
+int                 WiiRotation=0;
+int                 WiiDisplacementX=0;
+int                 WiiDisplacementY=0;
+#endif  // WII_CAMERA
+
+#endif  // USERHOOK_VARIABLES
+
+
